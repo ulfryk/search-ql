36f85d12872823e8e85d8e04f30c3c144f847e61
Jakub Strojewski <jakub.strojewski@gmail.com>
2018-05-11 09:02:07 +0200

    Update dependencies

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -5,15 +5,11 @@ const CircularDependencyPlugin = require('circular-dependency-plugin');
 module.exports = {
   mode: 'development',
   module: { rules: [{ test: /\.ts$/, loader: 'ts-loader', options: { configFile: 'tsconfig.json' } }] },
+  plugins: [
+    new CircularDependencyPlugin({ exclude: /node_modules/, failOnError: true }),
+  ],
   resolve: {
-
     modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-
     extensions: ['.ts', '.js'],
-
-    plugins: [
-      new CircularDependencyPlugin({ exclude: /node_modules/, failOnError: true }),
-    ],
-
   },
 };
