9fdb997af1e0b1cfef34e2bd3bd0b599db3eb3ff
Jakub Strojewski <jakub.strojewski@gmail.com>
2018-08-29 09:44:12 +0200

    PC-1293: SyntaxSugar: Better describe 'NOT' -> 'AND NOT'

--- src/ast/expressions/index.ts
+++ src/ast/expressions/index.ts
@@ -1,3 +1,4 @@
+export * from './and-not-sugar';
 export * from './binary-operation';
 export * from './expression';
 export * from './not';
--- src/ast/from-pairs.ts
+++ src/ast/from-pairs.ts
@@ -1,8 +1,8 @@
 import { Maybe, None, Some } from 'monet';
 
 import { SyntaxConfig } from '../config';
-import { BinaryOperationExpression, Expression, NotExpression, TermExpression } from './expressions';
-import { BinaryOperator, Operator } from './operators';
+import { AndNotSugarExpression, BinaryOperationExpression, Expression, TermExpression } from './expressions';
+import { BinaryOperator, NotOperator, Operator } from './operators';
 
 // TODO: Add operator precedence
 export const fromPairs =
@@ -11,13 +11,16 @@ export const fromPairs =
       .reduce((acc, [token, rhs]) => acc
           .flatMap(lhs => token
             .map(Operator.fromToken(config))
-            .map<Expression>(operator =>
-              // Not binary operator with LHS and RHS seems to be an error
-              operator.is(BinaryOperator) ?
-                BinaryOperationExpression.fromPair(operator)(lhs, rhs) :
-                // TODO: remove or re-implement AND-less NotExpression
-                // FIXME: …a pair with an unary operator?!?
-                BinaryOperationExpression.and(config.AND[0])(lhs, new NotExpression(rhs))))
+            .map<Expression>(operator => {
+              if (operator.is(BinaryOperator)) {
+                return BinaryOperationExpression.fromPair(operator)(lhs, rhs);
+              }
+              // The only Unary operator that can appear in `pairs` is `NotOperator`
+              if (operator.is(NotOperator)) {
+                return AndNotSugarExpression.create(config)(lhs, rhs);
+              }
+              throw Error(`Invalid operator pair: ${operator}`);
+            }))
           .orElse(Some(rhs)),
         None<Expression>())
       .orJust(TermExpression.empty()) // Should be validated as empty/ `InvalidExpression` maybe?
